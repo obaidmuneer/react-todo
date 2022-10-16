a0bcc902f5f0cf2b81c5761bc5e89f1ead9ae5c1
obaidmuneer <obaidmuneer55@gmail.com>
2022-10-16 09:51:58 -0700

    added class based filter

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import './App.css';
 import List from "./components/list";
 
 function App() {
   const [todos, setTodos] = useState([])
   const [item, setItems] = useState('')
+  const [course, setCourse] = useState('')
   const [btnText, setBtnText] = useState('Add')
   const [id, setId] = useState(-1)
 
+  useEffect(() => {
+    if (localStorage.getItem('course')) {
+      setCourse(localStorage.getItem('course'))
+    }
+    return
+  }, [])
+
+  let addCourse = (e) => {
+    e.preventDefault()
+    if (!course) {
+      return
+    }
+    // console.log(course);
+    localStorage.setItem('course', course)
+  }
+
   let add = (e) => {
     e.preventDefault()
-   if (!item) {
-     return
-   }
-    setTodos([...todos, item])
+    if (!item) {
+      return
+    }
+    setTodos([...todos, { course, text: item }])
+    console.log(todos);
     setItems('')
   }
   let edit = (i) => {
-    setItems(todos[i])
-    setBtnText('Update')
-    setId(i)
+    console.log(todos);
+    console.log(i);
+    // setItems(todos[i].text)
+    // setBtnText('Update')
+    // setId(i)
   }
   let update = (e) => {
     e.preventDefault()
     let editedItem = [...todos]
-    editedItem[id] = item
+    // console.log(editedItem[id].text);
+    editedItem[id].text = item
     setTodos(editedItem)
     setBtnText('Add')
     setItems('')
@@ -38,29 +59,44 @@ function App() {
   }
   return (
     <div className="container">
-      <div class="header">
-        <h2>Simple Todo App</h2>
+      <div className="header">
+        <h2>Sysborg Clone</h2>
       </div>
       <div className="body">
-        <form onSubmit={(id > -1) ? update : add} >
+        <form onSubmit={addCourse}>
+          <input className="item" onChange={(e) => setCourse(e.target.value)} value={course} type="text" />
+          <button className="btn add itemButton" type="submit">Add Course</button>
+        </form><br />
+        <form onSubmit={(id > -1) ? update : add}>
           <input className="item" onChange={(e) => setItems(e.target.value)} value={item} type="text" />
-          <button className="btn add itemButton" type="submit">{btnText}</button>
+          <button className="btn add itemButton">{btnText}</button>
         </form>
         <ul>
           {
-            todos.map((item, index) => {
+            todos.filter((item, index) => {
+              return item.course === course
+            }).map((item, index) => {
               return <List
                 key={index}
                 id={index}
-                text={item}
+                text={item.text}
                 onEdit={edit}
                 onDel={del}
               />
             })
+            // selectedCourse.map((item, index) => {
+            //   return <List
+            //     key={index}
+            //     id={index}
+            //     text={item.text}
+            //     onEdit={edit}
+            //     onDel={del}
+            //   />
+            // })
           }
         </ul>
       </div>
-    </div>
+    </div >
   );
 }
 
